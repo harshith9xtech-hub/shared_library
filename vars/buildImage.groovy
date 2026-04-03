80a1ce3b14ada74d26e8e8b18086885ae74d43f1
harshith9xtech-hub <harshith.9xtech@gmail.com>
2026-04-03 18:27:35 +0400

    corrected groovy file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,9 @@
 def call() {
-    sh 'docker build --label maintainer=cloudwithvarjosh@gmail.com -t "$REPO:$TAG" .'
-    docker push $DOCKER_IMAGE:$TAG
-    docker tag $DOCKER_IMAGE:$TAG $DOCKER_IMAGE:latest
-    docker push $DOCKER_IMAGE:lates
+    sh '''docker build --label maintainer=harshith.shivamoggi@gmail.com -t "$REPO:$TAG" .
+          docker tag $REPO:$TAG $DOCKER_IMAGE:$TAG
+          docker push $DOCKER_IMAGE:$TAG
+          docker tag $DOCKER_IMAGE:$TAG $DOCKER_IMAGE:latest
+          docker push $DOCKER_IMAGE:latest
+      '''
 
 }
