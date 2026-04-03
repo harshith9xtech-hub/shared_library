132b313accc411829df31454a6e9c1e4a511f74d
harshith9xtech-hub <harshith.9xtech@gmail.com>
2026-04-03 18:21:39 +0400

    edited buildgroovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,3 +1,7 @@
 def call() {
     sh 'docker build --label maintainer=cloudwithvarjosh@gmail.com -t "$REPO:$TAG" .'
+    docker push $DOCKER_IMAGE:$TAG
+    docker tag $DOCKER_IMAGE:$TAG $DOCKER_IMAGE:latest
+    docker push $DOCKER_IMAGE:lates
+
 }
