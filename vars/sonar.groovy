d721f42d4950968061845d35fac2a15af0a768ef
harshith9xtech-hub <harshith.9xtech@gmail.com>
2026-04-16 12:25:25 +0400

    sady

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,13 +1,11 @@
 def call() {
+    def scannerHome = tool 'sonar-scanner'
+
     withSonarQubeEnv('sonar') {
-        withCredentials([string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')]) {
-            sh """
-               sonar-scanner \
-               -Dsonar.projectKey=test-python \
-               -Dsonar.sources=. \
-               -Dsonar.host.url=http://localhost:9000 \
-               -Dsonar.login=sqp_450196e311232642eb7ce5ec7e3e2dc42a7ef95c
-            """
-        }
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=test-python \
+            -Dsonar.sources=.
+        """
     }
 }
