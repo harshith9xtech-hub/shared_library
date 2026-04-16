a151636ff72ecf61ad1b3d0e33f72b9487c911ce
harshith9xtech-hub <harshith.9xtech@gmail.com>
2026-04-16 12:51:46 +0400

    avshgvh

--- vars/pushImage.groovy
+++ vars/pushImage.groovy
@@ -1,5 +1,10 @@
 def call() {
+    if (!env.DOCKER_IMAGE || !env.TAG) {
+        error "DOCKER_IMAGE or TAG is not set"
+    }
+
     sh """
+        echo "Pushing image: ${env.DOCKER_IMAGE}:${env.TAG}"
         docker push ${env.DOCKER_IMAGE}:${env.TAG}
     """
 }
